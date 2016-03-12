757c8939229d37f81917c1844e237adcd054b97c
pschuler78 <patrick@schuler-it.ch>
2016-03-12 17:43:35 +0100

    cleaned up FlightInvoiceLineItem class

--- src/FLS.Data.WebApi/Invoicing/FlightInvoiceLineItem.cs
+++ src/FLS.Data.WebApi/Invoicing/FlightInvoiceLineItem.cs
@@ -16,9 +16,6 @@ namespace FLS.Data.WebApi.Invoicing
 
         public decimal Quantity { get; set; }
 
-        //public decimal PercentDiscount { get; set; }
-
-        //TODO: String or int as type
         public string UnitType { get; set; }
     }
 }
