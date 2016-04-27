7a6965a195e6efd3572354a62e84ccf0a49d684c
pschuler78 <patrick@schuler-it.ch>
2016-04-27 22:25:34 +0200

    Removed old InvoiceService logic

--- src/FLS.Server.Service/Invoicing/InvoiceRecipientTarget.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FLS.Server.Service.Invoicing
-{
-    internal class InvoiceRecipientTarget
-    {
-        internal string DisplayName { get; set; }
-        internal string MemberNumber { get; set; }
-        internal string MemberKey { get; set; }
-    }
-}
