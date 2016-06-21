cc5be794903942a4ef71a5d3095c2ae495b9b2ac
Roman Schlegel <roman.schlegel@gmail.com>
2016-06-21 18:34:58 +0200

    #67 moved notification settings to client related tab

--- database/FLSTest/3 insert/10 insert internationalisation values.sql
+++ database/FLSTest/3 insert/10 insert internationalisation values.sql
@@ -851,6 +851,9 @@ INSERT INTO [dbo].[LanguageTranslations] ([LanguageTranslationId], [TranslationK
 INSERT INTO [dbo].[LanguageTranslations] ([LanguageTranslationId], [TranslationKey], [TranslationValue], [LanguageId], [CreatedOn], [CreatedByUserId], [RecordState], [OwnerId], [OwnershipType], [IsDeleted]) 
      VALUES (NEWID(), 'ALL', 'Alle', 1, SYSDATETIME(), @insertUserId, 1, @ownerId, @OwnerShipType, 0)
 
+INSERT INTO [dbo].[LanguageTranslations] ([LanguageTranslationId], [TranslationKey], [TranslationValue], [LanguageId], [CreatedOn], [CreatedByUserId], [RecordState], [OwnerId], [OwnershipType], [IsDeleted]) 
+     VALUES (NEWID(), 'FLIGHT_PERMISSION', 'Flugerlaubnis', 1, SYSDATETIME(), @insertUserId, 1, @ownerId, @OwnerShipType, 0)
+
 GO
 
 
