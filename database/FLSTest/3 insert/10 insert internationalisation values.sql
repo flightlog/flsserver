b3699c63147a3e52eba07aea0d2e891088d568e5
Roman Schlegel <roman.schlegel@gmail.com>
2017-06-07 21:34:14 +0200

    added resend token features

--- database/FLSTest/3 insert/10 insert internationalisation values.sql
+++ database/FLSTest/3 insert/10 insert internationalisation values.sql
@@ -1115,6 +1115,9 @@ INSERT INTO [dbo].[LanguageTranslations] ([LanguageTranslationId], [TranslationK
 INSERT INTO [dbo].[LanguageTranslations] ([LanguageTranslationId], [TranslationKey], [TranslationValue], [LanguageId], [CreatedOn], [CreatedByUserId], [RecordState], [OwnerId], [OwnershipType], [IsDeleted])
      VALUES (NEWID(), 'RUN', 'Ausführen', 1, SYSDATETIME(), @insertUserId, 1, @ownerId, @OwnerShipType, 0)
 
+INSERT INTO [dbo].[LanguageTranslations] ([LanguageTranslationId], [TranslationKey], [TranslationValue], [LanguageId], [CreatedOn], [CreatedByUserId], [RecordState], [OwnerId], [OwnershipType], [IsDeleted])
+     VALUES (NEWID(), 'RESEND_EMAIL_TOKEN', 'Email-Bestätigung erneut senden', 1, SYSDATETIME(), @insertUserId, 1, @ownerId, @OwnerShipType, 0)
+
 GO
 
 
