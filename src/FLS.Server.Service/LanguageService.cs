0d3aa4de7093aa19f54e4ac462a0c72ad7ce29fc
pschuler78 <patrick@schuler-it.ch>
2017-09-07 22:24:42 +0200

    Redesigned LanguageService including server side translation methods

--- src/FLS.Server.Service/LanguageService.cs
+++ src/FLS.Server.Service/LanguageService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using FLS.Common.Exceptions;
 using FLS.Data.WebApi.Globalization;
 using FLS.Server.Data;
 using FLS.Server.Data.DbEntities;
@@ -23,23 +24,22 @@ namespace FLS.Server.Service
         #region Language
         public List<LanguageListItem> GetLanguageListItems()
         {
-            var dbEntityList = GetLanguages();
-
-            var list = dbEntityList.Select(language => language.ToLanguageListItem()).ToList();
-
-            return list;
-        }
-
-        internal List<Language> GetLanguages()
-        {
             using (var context = _dataAccessService.CreateDbContext())
             {
-                var list = context.Languages.OrderBy(c => c.LanguageName).ToList();
+                var languages = context.Languages
+                            .OrderBy(c => c.LanguageName)
+                            .Select(x => new LanguageListItem()
+                    {
+                        LanguageId = x.LanguageId,
+                        LanguageKey = x.LanguageKey,
+                        LanguageName = x.LanguageName
+                    })
+                    .ToList();
 
-                return list;
+                return languages;
             }
         }
-
+        
         internal Language GetLanguage(string languageKey)
         {
             using (var context = _dataAccessService.CreateDbContext())
@@ -54,31 +54,46 @@ namespace FLS.Server.Service
         #region Translation
         public Dictionary<string, string> GetTranslation(string languageKey)
         {
-            var language = GetLanguage(languageKey);
-
-            if (language == null) throw new Exception(string.Format("Language with key: {0} not found", languageKey));
-
-            var translations = GetTranslations(language.LanguageId);
-
-            var translation = new Translation();
-            translation.LanguageKey = languageKey;
-            translation.Translations = new Dictionary<string, string>();
-
-            foreach (var languageTranslation in translations)
+            using (var context = _dataAccessService.CreateDbContext())
             {
-                translation.Translations.Add(languageTranslation.TranslationKey, languageTranslation.TranslationValue);
-            }
+                var language = context.Languages.FirstOrDefault(l => l.LanguageKey.ToLower() == languageKey.ToLower());
 
-            return translation.Translations;
+                if (language == null)
+                    throw new Exception(string.Format("Language with key: {0} not found", languageKey));
+
+                var translations = context.LanguageTranslations
+                                    .Where(l => l.LanguageId == language.LanguageId)
+                                    .OrderBy(l => l.TranslationKey);
+
+                var translation = new Translation();
+                translation.LanguageKey = languageKey;
+                translation.Translations = new Dictionary<string, string>();
+
+                foreach (var languageTranslation in translations)
+                {
+                    translation.Translations.Add(languageTranslation.TranslationKey,
+                        languageTranslation.TranslationValue);
+                }
+
+                return translation.Translations;
+            }
         }
 
-        internal List<LanguageTranslation> GetTranslations(int languageId)
+        public string GetTranslation(string languageKey, string translationKey)
         {
             using (var context = _dataAccessService.CreateDbContext())
             {
-                var translations = context.LanguageTranslations.Where(l => l.LanguageId == languageId).OrderBy(l => l.TranslationKey);
+                var translation = context.LanguageTranslations
+                    .FirstOrDefault(l => l.Language.LanguageKey.ToLower() == languageKey.ToLower()
+                                         && l.TranslationKey.ToLower() == translationKey.ToLower());
 
-                return translations.ToList();
+                if (translation == null)
+                {
+                    Logger.Warn($"Language translation with key: {translationKey} for language: {languageKey} not found!");
+                    return translationKey.ToUpper();
+                }
+
+                return translation.TranslationValue;
             }
         }
         #endregion Translation
