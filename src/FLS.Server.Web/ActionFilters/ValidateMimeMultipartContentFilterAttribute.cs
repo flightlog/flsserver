11a4fddab43ec6e3720eba27d9e16425d7625ab1
pschuler78 <patrick@schuler-it.ch>
2017-12-07 20:40:03 +0100

    Fixed project reference issue for new AccountingUnitTypesController Added MimeMultipartContentFilterAttribute

---
+++ src/FLS.Server.Web/ActionFilters/ValidateMimeMultipartContentFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace FLS.Server.WebApi.ActionFilters
+{
+    public class ValidateMimeMultipartContentFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!actionContext.Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+
+        }
+
+    }
+}
