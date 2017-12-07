7d7091aac3aa9f37caa1348eaee88998fca249ff
pschuler78 <patrick@schuler-it.ch>
2017-12-07 20:30:38 +0100

    Added AccountingUnitType WebApi

---
+++ src/FLS.Data.WebApi/Accounting/AccountingUnitTypeListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FLS.Data.WebApi.Accounting
+{
+    public class AccountingUnitTypeListItem 
+    {
+        /// <summary>
+        /// Gets the Id of the object. The Id is set by the server.
+        /// </summary>
+        public int AccountingUnitTypeId { get; set; }
+
+        public string AccountingUnitTypeName { get; set; }
+    }
+}
